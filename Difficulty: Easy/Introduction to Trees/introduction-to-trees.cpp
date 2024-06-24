dabe41164851234aa427330f6056a4eec52403b6
Aditi Chauhan <116872023+Aditii004@users.noreply.github.com>
2024-06-24 23:49:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Trees/introduction-to-trees.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        int nodes=1;
+        int n;
+        for(n=1;n<i;n++){
+            nodes=nodes*2;
+        }
+        return nodes;
+        // your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
